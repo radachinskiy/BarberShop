76f75bb04dd367b8fc7103f3a12f24a712b3092e
MacBook Pro 15 <macbookpro15@MacBook-Pro-MacBook.local>
2023-03-19 17:54:29 +0300

    Remove hardcoded barbers list

--- app.rb
+++ app.rb
@@ -7,10 +7,23 @@ def is_barber_exists? db, name
 	db.execute('select * from Barbers where name=?', [name]).length > 0
 end
 
+def seed_db db, barbers
+	barbers.each do |barber| 
+		if !is_barber_exists? db, barber
+			db.execute 'insert into Barbers (name) values (?)', [barber]
+		end
+	end
+end
+
 def get_db
 	db = SQLite3::Database.new 'barbershop.db'
 	db.results_as_hash = true
 	return db
+end
+
+before do 
+	db = get_db
+	@barbers = db.execute 'select * from Barbers'
 end
 
 configure do
@@ -32,6 +45,8 @@ configure do
 		"Name"	TEXT,
 		PRIMARY KEY("Id" AUTOINCREMENT)
 		)'
+
+	seed_db db, ['Jessie Pinkman', 'Walter White', 'Gus Fring', 'Mike Ehrmantraut']
 end
 
 get '/' do
